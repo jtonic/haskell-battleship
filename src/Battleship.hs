1cfcd89f292d6d3c498f7b29c12e000f4ea8572b
Antonel-Ernest Pazargic <antonel-ernest.pazargic@ing.com>
2021-06-21 20:59:06 +0300

    Some simple handle, lense examples

--- src/Battleship.hs
+++ src/Battleship.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Battleship (
     run
@@ -23,26 +23,36 @@ run = do
             , appDescription = "Battleship game"
             , appHandle = stderr
             }
-    runRIO app $ do
-        startApp >> showTime
-        stopApp
+    runRIO app $ startApp >> sayTime >> stopApp
 
 startApp :: RIO App ()
 startApp = do
     App name desc _ <- ask
-    say $ "Stopping the app." <> " Name: " <> name <> ", desc: " <> desc
+    say $ "Starting the app." <> " Name: " <> name <> ", desc: " <> desc
 
 stopApp :: RIO App ()
 stopApp = do
     App name desc _ <- ask
     say $ "Stopping the app." <> " Name: " <> name <> ", desc: " <> desc
 
-showTime :: RIO App ()
-showTime = do
+sayTime :: HasHandle env => RIO env ()
+sayTime = do
     now <- getCurrentTime
     say $ "Started at time: " <> pack (show now)
 
-say :: Text -> RIO App ()
+say :: HasHandle env => Text -> RIO env ()
 say msg = do
-    App _ _ h <- ask
+    h <- view handleL
     liftIO $ hPutStrLn h $ unpack msg
+
+switchHandle :: HasHandle env => Handle -> RIO App env -> RIO App env
+switchHandle h = local (set handleL h)
+
+class HasHandle env where
+    handleL :: Lens' env Handle
+
+instance HasHandle Handle where
+    handleL = id
+
+instance HasHandle App where
+    handleL = lens appHandle (\x y -> x {appHandle = y} )
